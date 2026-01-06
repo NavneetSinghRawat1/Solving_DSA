bfe7d722e525a0c34c90da8f982eeca7bf394cf7
NavneetSinghRawat1 <navneetsinghrawat8@gmail.com>
2026-01-06 18:42:48 +0530

    updated

---
+++ 1390.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> nums={21,21};
+    int sum=0;
+    for(int i=0;i<nums.size();i++){
+        vector<int> temp;
+        int n=nums[i],temp_sum=0;
+        for(int j=1;j*j<=n;j++){
+            if(n%j==0){
+                temp.push_back(j);
+                temp_sum+=j;
+                if(j*j!=n){
+                    temp.push_back(n/j);
+                    temp_sum+=n/j;
+                }
+            }
+        }
+        if(temp.size()==4)sum+=temp_sum;
+    }
+    cout<<sum;
+}
