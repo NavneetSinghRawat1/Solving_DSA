d0c21d9945721e668cbc647bf36f5a9b84323cd5
NavneetSinghRawat1 <navneetsinghrawat8@gmail.com>
2026-03-26 10:52:57 +0530

    updated

---
+++ 700.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+TreeNode* searchBST(TreeNode* root, int val) {
+    TreeNode* temp=root;
+    while(temp){
+        if(temp->val==val){
+            return temp;
+        }
+        else if(temp->val>val){
+            temp=temp->left;
+        }
+        else{
+            temp=temp->right;
+        }
+    }
+    return NULL;
+}
+int main(){
+    
+}
